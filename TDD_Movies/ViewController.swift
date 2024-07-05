6b5b31c86ba87d80e635f152e51fe9dd665f4f81
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2024-07-05 14:47:44 +0200

    Initial Commit

---
+++ TDD_Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TDD_Movies
+//
+//  Created by Miguel Gallego on 5/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TDD_MoviesUITests/TDD_MoviesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TDD_MoviesUITestsLaunchTests.swift
+//  TDD_MoviesUITests
+//
+//  Created by Miguel Gallego on 5/7/24.
+//
+
+import XCTest
+
+final class TDD_MoviesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
